398be30f2827e0c93199bf67d91b5c6b3fb43f5e
Lucas <lucasazzollinivieira@gmail.com>
2022-01-20 18:24:21 -0300

    refatoring the /is-today-holiday route to be easier to understand

--- index.js
+++ index.js
@@ -6,44 +6,30 @@ import { holidays } from "./holidays.js";
 
 const app = express();
 const port = 5000;
-app.use(cors());
+app.use(cors);
 
 app.get("/holidays", (request, response) => {
     response.send(holidays);
 });
 
 app.get("/is-today-holiday", (request, response) => {
-    response.send(IsTodayHoliday(holidays));
+    const today = new Date();
+
+    const todayHoliday = holidays.find(holiday => holiday.date === today.toLocaleDateString());
+
+    if (todayHoliday) {
+        response.send(`Sim, hoje é ${holidays.name}`);
+    } else {
+        response.send(`Não, hoje não é feriado`);
+    }
 });
 
-// app.get("holiday/:id", (request, response) => {
-//     response.send(ListHolidays(holidays, request.params.id));
-// });
+app.get("holidays/:month", (request, response) => {
+    const monthHolidays = holidays.filter(holiday => holiday.date.split("/")[0] === request.params.month)
+
+    response.send(monthHolidays);
+})
 
 app.listen(port, () => {
-    console.log(`Servidor ${chalk.bgGreen(chalk.black(' ON '))} - Porta ${chalk.blue(port)}`);
+    console.log(`Servidor ${chalk.bgGreen(chalk.black(' ON '))} - Porta ${chalk.magenta(port)} - ${chalk.blue(`http://localhost:${port}/holidays`)}`);
 });
-
-// function ListHolidays(holidays, id) {
-//     return holidays.filter(holiday => holiday.date.split("/")[0] === id);
-// }
-
-function IsTodayHoliday(day) {
-    const today = new Date();
-    let holiday = {
-        isHoliday: false,
-        name: "",
-    };
-
-    function setHoliday(name) {
-        holiday.isHoliday = true;
-        holiday.name = name;
-    }
-
-    day.forEach(holy => {
-        holy.date === today.toLocaleDateString('en-US') && setHoliday(holy.name)
-    });
-
-    return holiday.isHoliday ? `Sim, hoje é ${holiday.name}` : `Não, hoje não é feriado`;
-}
-
